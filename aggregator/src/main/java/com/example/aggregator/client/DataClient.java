b7553977e2ec998213f0e1ef2895efa37f710cce
twister1803 <mvchekalin@mail.ru>
2021-10-19 13:39:10 +0500

    fixed #4

--- aggregator/src/main/java/com/example/aggregator/client/DataClient.java
+++ aggregator/src/main/java/com/example/aggregator/client/DataClient.java
@@ -3,13 +3,13 @@ package com.example.aggregator.client;
 import com.example.aggregator.dto.PaymentResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @FeignClient(value = "data")
 public interface DataClient {
-    @GetMapping("/api/payments?page={page}&count={count}")
-    List<PaymentResponseDto> getPayments(@PathVariable int page, @PathVariable int count);
+    @GetMapping("/api/payments")
+    List<PaymentResponseDto> getPayments(@RequestParam(value = "page") int page, @RequestParam(value = "count") int count);
 
 }
